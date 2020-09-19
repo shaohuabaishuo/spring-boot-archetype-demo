572157dd672111c32a6824169eb42ac7bdd67b82
jingzhong <jingzhong>
2020-09-19 17:55:08 +0800

    1、创建脚手架的例子

---
+++ spring-boot-archetype-app-service/src/main/java/com/jz/StartMain.java
@@ -0,0 +1,26 @@
+package com.jz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableFeignClients
+@EnableHystrix
+@ComponentScan(value = {"com.jz"})
+@SpringBootApplication
+@EnableDiscoveryClient(autoRegister = false)
+@EnableScheduling
+public class StartMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartMain.class, args);
+    }
+
+}
